eafd7f94de0103848218fb25405bb27b607d14c7
Marko Kostiv <>
2018-07-04 15:03:43 +0300

    Sixth commit

--- Train.py
+++ Train.py
@@ -1,6 +1,6 @@
 from NeuralNet import *
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss
+import matplotlib.pyplot as plt
 
 class Trainer:
 
@@ -10,6 +10,7 @@ class Trainer:
         self.learning_rates = learning_rates
         self.batch_sizes = batch_sizes
         self.number_of_epochs = number_of_epochs
+        self.validation_loss = []
 
     def data_split(self):
         train = self.data
@@ -17,18 +18,40 @@ class Trainer:
         X_train, X_test, y_train, y_test = train_test_split(train, true_labels, test_size=0.25, random_state=42)
         return X_train, X_test, y_train, y_test
 
-    @staticmethod
-    def indices_to_one_hot(data, nb_classes):
+    def indices_to_one_hot(self, data, nb_classes):
         targets = np.array(data).reshape(-1)
         return np.eye(nb_classes)[targets]
 
+    def loss(self, predictions, y_hat):
+        self.validation_loss.append(log_loss(y_hat, predictions))
+
     def fit(self, model):
         X_train, X_test, y_train, y_test = self.data_split()
+        y_test_hat = self.indices_to_one_hot(y_test, 2)
 
         for i in range(self.number_of_epochs):
             output = model.forward(X_train)
             y_hat = self.indices_to_one_hot(y_train, 2)
-            model.backward(y_hat, output, X_train)
+            model.backward(output, y_hat, X_train)
+            self.loss(model.forward(X_test), y_test_hat)
+
+        self.plot_model(model.loss, self.validation_loss)
+
+    def plot_model(self, loss, val_loss):
+        # Create sub-plots
+        fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+
+        # Summarize history for accuracy
+        axs[0].plot(range(1, len(loss) + 1), loss)
+        axs[0].plot(range(1, len(val_loss) + 1), val_loss)
+        axs[0].set_title('Model Loss')
+        axs[0].set_ylabel('Loss')
+        axs[0].set_xlabel('Epoch')
+        axs[0].set_xticks(np.arange(1, len(val_loss) + 1), len(val_loss) / 10)
+        axs[0].legend(['train', 'val'], loc='best')
+
+        # Show the plot
+        plt.show()
 
     def predict(self, data, model):
         predictions = model.forward(data)
@@ -57,7 +80,7 @@ layer1 = Layer(num_of_features, dim_hidden_1, tanh_f)
 layer2 = SkipLayer(dim_hidden_1, num_of_features, input)
 layer3 = Layer(dim_hidden_1, dim_hidden_3, softmax)
 
-network = NeuralNet([layer, layer1, layer2, layer3], skip_layer=layer2, learning_rate=0.01)
+network = NeuralNet([layer, layer1, layer2, layer3], skip_layer=layer2, learning_rate=0.001)
 
-trainer = Trainer(data.values, labels.values, [0.1, 0.001, 0.0001], [10, 20, 50], 100)
+trainer = Trainer(data.values, labels.values, [0.1, 0.001, 0.0001], [10, 20, 50], 1000)
 trainer.fit(model=network)
